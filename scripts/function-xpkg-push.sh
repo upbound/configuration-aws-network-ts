ba6acb7d57e3bcdd1ba265d2fcfed18db5a3cc7a
Steven Borrelli <steve@borrelli.org>
2026-01-14 15:27:29 +0000

    error if package files don't exist
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- scripts/function-xpkg-push.sh
+++ scripts/function-xpkg-push.sh
@@ -4,4 +4,9 @@
 
 set -xe
 XPKG_FILES=$(echo ${XPKG_DIR}/${FN_NAME}-*-v${VERSION}.xpkg|tr ' ' ,)
-up xpkg push ${XPKG_REPO}/${FN_NAME}:v${VERSION} -f ${XPKG_FILES}
+XPKG_FILES=$(echo "${XPKG_DIR}/${FN_NAME}"-*-v"${VERSION}".xpkg | tr ' ' ',')
+if [ -z "${XPKG_FILES}" ] || [ "${XPKG_FILES}" = "${XPKG_DIR}/${FN_NAME}-*-v${VERSION}.xpkg" ]; then
+  echo "Error: No xpkg files found matching pattern"
+  exit 1
+fi
+up xpkg push "${XPKG_REPO}/${FN_NAME}:v${VERSION}" -f "${XPKG_FILES}"
